c4712b5970f753fcd9494d3a7c3bcd5ece719096
Scorti-Paul <scortip@gmail.com>
2025-01-10 11:32:38 +0000

    array methods

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+/* 
 function logger() {
   console.log('running a new function')
 }
@@ -41,3 +42,67 @@ const retirementYearLeft = (birthYear) => {
 
 
 console.log('Retire: ' + retirementYearLeft(1995));
+*/
+
+
+const certs = [
+  'degree',
+  'hdn',
+  'diploma',
+  'phd'
+]
+console.log(certs);
+
+/**
+ *  Add value to the start of the array
+ *  returns the length of the array
+ */ 
+console.log(certs.unshift('certificate'));
+console.log(certs);
+
+/**
+ *  Add value to the end of the array
+ *  returns the length of the array
+ */ 
+console.log(certs.push('advance diploma'));
+console.log(certs);
+
+/**
+ *  remove value from the start of the array
+ *  returns a string of the value removed
+ */ 
+console.log(certs.shift())
+console.log(certs)
+
+/**
+ *  remove value from the end of the array
+ *  returns a string of the value removed
+ */ 
+console.log(certs.pop());
+console.log(certs);
+
+
+console.log(`IndexOf: ${certs.indexOf('phd')}`)
+console.log(`Includes: ${certs.includes('phd')}`)
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Jay',
+  birthYear: 1995,
+  job: 'Software Engineer',
+  friends: ['Martins', 'Andrews', 'Nat'],
+
+  calcAge: function() {
+    this.age = 2025 - this.birthYear;
+    return this.age;
+  }
+}
+
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends')
+// console.log(typeof jonas[interestedIn])
+
+// jonas[interestedIn] ? console.log(jonas[interestedIn]) : console.log('Values not available')
+
+// console.log(`Jonas has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[2]}`)
+
+console.log('Jonas Age: ' + jonas.age)
