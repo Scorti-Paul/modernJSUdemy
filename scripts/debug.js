3069d3e0cf2fff96ea9315412156f52a757f09c9
Scorti-Paul <scortip@gmail.com>
2025-01-17 21:48:56 +0000

    refactor

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,13 +1,16 @@
 const printForecast = (arr) => {
+  let reportForecast = "";
   for (let i = 0; i < arr.length; i++) {
     const counter = i + 1;
-    console.log(
-      `...${arr[i]}°C in ${counter} ${counter === 1 ? "day" : "days"}.`
-    );
+    // const counter = arr.indexOf(arr[i]) + 1;
+    reportForecast += `...${arr[i]}°C in ${counter} ${
+      counter === 1 ? "day" : "days"
+    } `;
   }
+  return reportForecast;
 };
 
-// const temperature = [17, 21, 23];
-const temperature = [12, 5, -5, 0, 4];
+const temperature = [17, 21, 23];
+// const temperature = [12, 5, -5, 0, 4];
 
-printForecast(temperature);
+console.log(printForecast(temperature));
